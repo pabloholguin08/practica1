b550a62fd8374410d2e9b940956cc645a51b1a9a
pabloholguin08 <jpholguin06@gmail.com>
2021-07-21 16:53:04 -0500

    Initial commit

---
+++ controlador.js
@@ -0,0 +1,7 @@
+// Hacer un programa que nos ayude a establecer el costo total de una consola de videojuegos
+/*La consola se va a compar en el exterior - Compra en dolares
+→Pagar un casillero (dolares dependiendo cantidad de productos)
+→Pagar envío (cantidad de productos)
+→Impuesto de entrada (lo dígita aduanas)
+*/
+scroll.
